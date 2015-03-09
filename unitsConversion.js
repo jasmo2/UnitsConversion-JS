e001af7df113b0fe4b9de79b8271d5e31baed330
jasmo2 <jasmo2.com@gmail.com>
2015-03-09 14:43:03 -0500

    add logic with JS

---
+++ unitsConversion.js
@@ -0,0 +1,62 @@
+window.onload = function() {
+	buttonCalculate = document.getElementById("calculate");
+	buttonCalculate.onclick = function() {
+        var item1 = document.getElementsByName("selector")[0].options[document.getElementsByName("selector")[0].selectedIndex].value
+        var item2 = document.getElementsByName("selector")[1].options[document.getElementsByName("selector")[1].selectedIndex].value
+		var longitud = new Longitud(document.getElementById("valor"),item1,item2);
+        document.getElementById("ResultText").value = longitud.calculate();
+		document.getElementById("units").value = item2;
+		
+	};
+};
+function Longitud(values,firstUnit,secondUnit) {
+    this.values = values;
+    this.firstUnit = typeof firstUnit !== 'undefined' ? firstUnit : "";
+    this.secondUnit = typeof secondUnit !== 'undefined' ? secondUnit : "";
+    Longitud.prototype = {
+        isNumeric: function( n ) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        },
+        calculate: function() {
+            if (this.isNumeric(this.values.value)) {
+               this.values = this.values.value 
+               var firstValue = 1;
+               var secondValue = 1;
+
+               switch(this.firstUnit){
+                    case "m":
+                        firstValue = 1 ;
+                        break;
+                     case "cm":
+                        firstValue = 1/100 ;
+                        break;
+                     case "mm":
+                        firstValue = 1/1000 ;
+                        break;
+               };
+               switch(this.secondUnit){
+                    case "m":
+                        secondValue = 1 ;
+                        break;
+                     case "cm":
+                        secondValue = 100 ;
+                        break;
+                     case "mm":
+                        secondValue = 1000 ;
+                        break;
+               };
+               return firstValue*secondValue
+            }else{
+                alert("please enter a number");
+                this.values = null;
+            };
+        },
+        setFirstUnit: function (value){
+            this.firstUnit = value;
+        },
+        setSecondUnit: function (value){
+            this.secondUnit = value;
+        }
+
+    };
+};
